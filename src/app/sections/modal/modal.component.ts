819a8062f4e213156a60326d10e7999850037d97
Samakunchan <samakunchan@gmail.com>
2019-09-25 20:09:34 +0200

    Feat: Config commit
    - Mise à jour des érreurs TSLINT
    - Mise à jour de package.json pour le bon nom de projet
    - Installation de Husky
    - Création tuto PRECOMMIT
    - Correction TSLINT max-line = 150

--- src/app/sections/modal/modal.component.ts
+++ src/app/sections/modal/modal.component.ts
@@ -11,14 +11,10 @@ import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
       </button>
     </div>
     <div class="modal-body">
-      Far far away, behind the word mountains,
-      far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in
-      Bookmarksgrove right at the coast of the Semantics,
-      a large language ocean. A small river named Duden flows by their place and supplies it with
-      the necessary regelialia. It is a paradisematic country,
-      in which roasted parts of sentences fly into your mouth.
-      Even the all-powerful Pointing has no control about the blind texts it is an almost
-      unorthographic life One day however a small line of blind text by the name of Lorem Ipsum
+      Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in
+      Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with
+      the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing
+      has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum
       decided to leave for the far World of Grammar.
     </div>
     <div class="modal-footer">
