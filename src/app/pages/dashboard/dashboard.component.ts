ec125d13a411be5b6fd6df15f0496e109b2ee531
Samakunchan <samakunchan@gmail.com>
2019-09-26 23:59:43 +0200

    style(fin): dashboard
    - voir commit 1 et 2

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -21,19 +21,19 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     // this.datasets = [[0, 20, 10, 30, 15, 40, 20, 60, 60], [0, 20, 5, 25, 10, 30, 15, 40, 40]];
     // this.data = this.datasets[0];
-//
+    //
     // const chartOrders = document.getElementById('chart-orders');
-//
+    //
     // parseOptions(Chart, chartOptions());
-//
+    //
     // const ordersChart = new Chart(chartOrders, {
     //   type: 'bar',
     //   options: chartExample2.options,
     //   data: chartExample2.data,
     // });
-//
+    //
     // const chartSales = document.getElementById('chart-sales');
-//
+    //
     // this.salesChart = new Chart(chartSales, {
     //   type: 'line',
     //   options: chartExample1.options,
