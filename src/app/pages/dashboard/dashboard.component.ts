ccc26a4fa10b2011b20962e31c97b0bf7f4dd47a
Samakunchan <samakunchan@gmail.com>
2019-09-26 23:57:38 +0200

    style(part2): dashboard
    - Image image layout du cours suivi
    - Ajout flaticon1
    - Ajout flaticon2
    - Ajout fontawesome 5
    - Integration de vendor.bundle.rtl.css dans angular.json
    - Suppression des anciens fontawesome de index.html

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -19,30 +19,30 @@ export class DashboardComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.datasets = [[0, 20, 10, 30, 15, 40, 20, 60, 60], [0, 20, 5, 25, 10, 30, 15, 40, 40]];
-    this.data = this.datasets[0];
-
-    const chartOrders = document.getElementById('chart-orders');
-
-    parseOptions(Chart, chartOptions());
-
-    const ordersChart = new Chart(chartOrders, {
-      type: 'bar',
-      options: chartExample2.options,
-      data: chartExample2.data,
-    });
-
-    const chartSales = document.getElementById('chart-sales');
-
-    this.salesChart = new Chart(chartSales, {
-      type: 'line',
-      options: chartExample1.options,
-      data: chartExample1.data,
-    });
+    // this.datasets = [[0, 20, 10, 30, 15, 40, 20, 60, 60], [0, 20, 5, 25, 10, 30, 15, 40, 40]];
+    // this.data = this.datasets[0];
+//
+    // const chartOrders = document.getElementById('chart-orders');
+//
+    // parseOptions(Chart, chartOptions());
+//
+    // const ordersChart = new Chart(chartOrders, {
+    //   type: 'bar',
+    //   options: chartExample2.options,
+    //   data: chartExample2.data,
+    // });
+//
+    // const chartSales = document.getElementById('chart-sales');
+//
+    // this.salesChart = new Chart(chartSales, {
+    //   type: 'line',
+    //   options: chartExample1.options,
+    //   data: chartExample1.data,
+    // });
   }
 
-  public updateOptions() {
-    this.salesChart.data.datasets[0].data = this.data;
-    this.salesChart.update();
-  }
+  // public updateOptions() {
+  //   this.salesChart.data.datasets[0].data = this.data;
+  //   this.salesChart.update();
+  // }
 }
