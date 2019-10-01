6e30e628726d97d3c067c07cf6fb05c6904b2f83
Samakunchan <samakunchan@gmail.com>
2019-10-02 01:46:54 +0200

    style: auth design
    - Changement design login
    - Changement design register
    - Modification du footer front en plus sobre
    - Suppression de la navbar

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener, Renderer2, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,22 +10,21 @@ export class AuthLayoutComponent implements OnInit, OnDestroy {
   test: Date = new Date();
   public isCollapsed = true;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private renderer: Renderer2, private element: ElementRef) { }
 
   ngOnInit() {
     const html = document.getElementsByTagName('html')[0];
     html.classList.add('auth-layout');
     const body = document.getElementsByTagName('body')[0];
-    body.classList.add('bg-default');
+    body.classList.add('bg-blue');
     this.router.events.subscribe((event) => {
       this.isCollapsed = true;
    });
-
   }
   ngOnDestroy() {
     const html = document.getElementsByTagName('html')[0];
     html.classList.remove('auth-layout');
     const body = document.getElementsByTagName('body')[0];
-    body.classList.remove('bg-default');
+    body.classList.remove('bg-blue');
   }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
+  public front = {
+    logo: {
+      src: './assets/img/brand/logo-viodiz-mini.png',
+      alt: 'Logo',
+    },
+  };
   constructor() {}
 
   ngOnInit() {
