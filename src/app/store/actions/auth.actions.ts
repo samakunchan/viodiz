b5f6158b7f3f422070c79d714c0f139d33bb7a7b
Samakunchan <samakunchan@gmail.com>
2019-10-03 15:45:48 +0200

    feat(02): nrgx installation
    - Creation /actions/auth + config
    - Creation /actions/role
    - Creation /actions/permission
    - Creation /effects/auth
    - Creation /effects/role
    - Creation /effects/permission
    - Creation /reducers/auth
    - Creation /reducers/role
    - Creation /reducers/permission

---
+++ src/app/store/actions/auth.actions.ts
@@ -0,0 +1,46 @@
+import { Action } from '@ngrx/store';
+
+export enum AuthActionTypes {
+  Login = '[Login] Action',
+  Logout = '[Logout] Action',
+  Register = '[Register] Action',
+  UserRequested = '[Request User] Action',
+  UserLoaded = '[Load User] Auth API',
+  CurrentUserUpdatePhoto = '[Update current user photo] Auth API',
+  CurrentUserUpdateAddInfos = '[Update current user additionnal information] Auth API',
+}
+
+export class Login implements Action {
+  readonly type = AuthActionTypes.Login;
+  // Etape 3 de l'initialisation de l'app. Ensuite => auth.reducer.ts ligne 22
+  constructor(public payload: { authToken: string }) {}
+}
+
+export class Logout implements Action {
+  readonly type = AuthActionTypes.Logout;
+}
+
+export class Register implements Action {
+  readonly type = AuthActionTypes.Register;
+  constructor(public payload: { authToken: string }) {}
+}
+
+export class UserRequested implements Action {
+  readonly type = AuthActionTypes.UserRequested;
+}
+
+export class UserLoaded implements Action {
+  readonly type = AuthActionTypes.UserLoaded;
+  constructor(public payload: { user }) {} // rajouter le type User
+}
+
+export class CurrentUserUpdatePhoto implements Action {
+  readonly type = AuthActionTypes.CurrentUserUpdatePhoto;
+  constructor(public payload: { user }) {} // rajouter le type User
+}
+
+export class CurrentUserUpdateAddInfos {
+  readonly type = AuthActionTypes.CurrentUserUpdateAddInfos;
+  constructor(public payload: { user }) {} // rajouter le type User
+}
+export type AuthActions = Login | Logout | Register | UserRequested | UserLoaded | CurrentUserUpdatePhoto | CurrentUserUpdateAddInfos;
---
+++ src/app/store/effects/role.effects.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect } from '@ngrx/effects';
+import { defer, Observable } from 'rxjs';
+import { Action } from '@ngrx/store';
+
+@Injectable()
+export class RoleEffects {
+  @Effect()
+  init$: Observable<Action> = defer(() => {
+    // A changer
+    return this.actions$;
+  });
+  constructor(private actions$: Actions) {}
+}
