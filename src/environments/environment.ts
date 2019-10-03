a07f6b4cffd80d1e19d8ce46e95c6fe0457f3d66
Samakunchan <samakunchan@gmail.com>
2019-10-03 16:42:09 +0200

    feat(firebase01): installation
    - Installation des dépendences firebase (normal + angularFire)
    - Installation des cloud functions
    - Création du fichier de config firebase pour la connexion
    - Connexion de firebase au projet dev "samakunchan" (.firebaserc)
    - Création du fichier de déploiement firebase (firebase.json)
    - Mise à jour du gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 };
 
 /*
