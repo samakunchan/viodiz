e6644b4bb3de624f8b0b4eaa079790e68fb4782c
Samakunchan <samakunchan@gmail.com>
2019-10-11 03:26:21 +0200

    feat: traduction
    - mise à jour des fichiers i18n fr et en
    - Mise en place de la traduction sur le login
    - Mise en place de la traduction sur le passworForgot
    - Mise en place de la traduction sur le register

--- src/app/core/i18n/fr.ts
+++ src/app/core/i18n/fr.ts
@@ -2,8 +2,30 @@ export const locale = {
   lang: 'fr',
   data: {
     AUTH: {
-      TITLE: 'Bienvenu',
-      TEXT: 'Utilisez ce super formulaire pour vous connecter ou créer un compte simplement gratuitement',
+      LOGIN: {
+        SOCIAL: {
+          TEXT: 'Se connecter avec',
+        },
+        CRED: 'Ou connecter avec vos identifiants',
+        REMEMBER: 'Se rappeler de moi',
+        SIGININ: 'Se connecter',
+        FAILURE: 'Votre login ou le mot de passe est incorrect.',
+        PASSWORD: {
+          FORGOT: 'Mot de passe oublier?',
+          RECUP: 'Récupérez votre mot de passe en entrant votre email dans la champ ci-dessous.',
+          SEND: 'Envoyez',
+          RETURN: 'Retour vers la page de connexion',
+          SUCCESS: 'Votre compte a bien été mis à jour. Vérifier votre boite mail.',
+          FAILURE: 'Un problème est survenu lors de l\'envoie de votre email.',
+        },
+        NEWACC: 'Créer un nouveau compte',
+      },
+      REGISTER: {
+        CRED: 'Ou créer votre compte d\'utilisateur.',
+        AGREE: 'J\'accepter les conditions de la ',
+        POLICY: 'Politique Privée',
+        BTN: 'Créer un compte',
+      },
     },
     FRONT: {
       MENU: {
