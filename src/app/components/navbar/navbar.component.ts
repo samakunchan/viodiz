1dbccd525f6a7803c328eaa9751cdae6f1a18861
Samakunchan <samakunchan@gmail.com>
2019-10-13 02:44:51 +0200

    feat(nav-bar): affichage user
    - Integration du current user via le store
    - Integration Html du current user en async
    - Integration du pipe 'firstletter' dans le admin.module, et export dans le common.module
    - Utilisation du pipe 'firstletter' dans le cas ou la photo est manquante
    - Mise à jour scss pour le 'firstletter'

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,8 +4,10 @@ import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { Store } from '@ngrx/store';
-import { AppState } from '../../store';
+import { AppState, currentUser } from '../../store';
 import { Logout } from '../../store/actions/auth.actions';
+import { Observable } from 'rxjs';
+import { AuthUser } from '../../core/models/auth.model';
 
 @Component({
   selector: 'app-navbar',
@@ -16,6 +18,7 @@ export class NavbarComponent implements OnInit {
   public focus;
   public listTitles: any[];
   public location: Location;
+  public user$: Observable<AuthUser>;
   constructor(
     location: Location,
     private element: ElementRef,
@@ -28,20 +31,9 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
     this.listTitles = ROUTES.filter(listTitle => listTitle);
+    this.user$ = this.store.select(currentUser);
   }
-  getTitle() {
-    let titlee = this.location.prepareExternalUrl(this.location.path());
-    if (titlee.charAt(0) === '#') {
-      titlee = titlee.slice(1);
-    }
 
-    for (let item = 0; item < this.listTitles.length; item++) {
-      if (this.listTitles[item].path === titlee) {
-        return this.listTitles[item].title;
-      }
-    }
-    return 'Dashboard';
-  }
   getBg() {
     return 'bg-profil-bar';
   }
